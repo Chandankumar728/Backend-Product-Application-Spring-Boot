7413b76b47bdf878db178a3de09de6b81c9208d1
Chandankumar728 <chandankumar6299068@gmail.com>
2025-12-05 19:06:10 +0530

    swager implemented for api documentation

--- src/main/java/com/bitsnbuytes/product/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bitsnbuytes/product/exception/GlobalExceptionHandler.java
@@ -1,22 +1,40 @@
 package com.bitsnbuytes.product.exception;
 
+import com.bitsnbuytes.product.dto.ExceptionResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CategoryNotFoundExist.class)
-    public ResponseEntity<String> handleCategoryNotFound(CategoryNotFoundExist ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<ExceptionResponseDTO> handleCategoryNotFound(CategoryNotFoundExist ex, WebRequest webRequest) {
+
+        ExceptionResponseDTO response = new ExceptionResponseDTO(
+                webRequest.getDescription(false),
+                HttpStatus.NOT_FOUND,
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
-    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponseDTO> handleRuntimeException(RuntimeException ex, WebRequest webRequest) {
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+        ExceptionResponseDTO response = new ExceptionResponseDTO(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 }
